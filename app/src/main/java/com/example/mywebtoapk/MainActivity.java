c29eb6aea2d7035ad50cfdf0cb1f9d7c7b0ba814
kvaishali082-web <kvaishali082@gmail.com>
2026-01-29 07:47:30 +0530

    Create MainActivity to display web content
    Implement MainActivity to load a web page in a WebView.

---
+++ app/src/main/java/com/example/mywebtoapk/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mywebtoapk;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        // ITHE TUZI GITHUB LINK TAKA
+        webView.loadUrl("https://tuzi-github-link.com");
+    }
+}
